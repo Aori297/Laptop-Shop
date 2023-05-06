e0cbd602f0218a20921762b13c67ed4869d3f0bf
- [ a o r i ] - | 愛 | <shaswats.snm@gmail.com>
2023-05-06 14:19:03 +0545

    sell code

--- files/read.py
+++ files/read.py
@@ -1,1 +1,17 @@
 # reading the laptop.txt file here
+
+def readFile():
+    file = open("laptop.txt","r")
+    lap_dict = {}
+    lap_id = 1 
+    for line in file:
+        
+        # replace \n with ""
+        line = line.replace("\n","")
+
+        # updating it into the dictionary
+        lap_dict.update({lap_id: line.split(",")})
+        lap_id = lap_id + 1
+
+        # returns the values of laptop dictionary
+        return lap_dict
