3f4bc535480014e2a3bd5c1ef22c80b6c9aa0895
- [ a o r i ] - | 愛 | <shaswats.snm@gmail.com>
2023-05-09 10:44:38 +0545

    math milena

--- files/write.py
+++ files/write.py
@@ -12,6 +12,7 @@ def sellingBill(name, address, phone, choosenLaptop):
     print("The Notebook Warehouse".center(100))
     print("Basundhara, Kathmandu".center(100))
     print("\n")
+    print("Generating Bill...")
     print("---------------------------------------------------------------------------------------------------------")
     print("Name:", name)
     print("Address:", address)
@@ -30,7 +31,24 @@ def sellingBill(name, address, phone, choosenLaptop):
     print("Grand Total", grandTotal)
 
 
-def purchaseBill():
+def purchaseBill(distributername, choosenLaptop):
+    total = 0
+    for i in choosenLaptop:
+        total += int(i[3])
+    totalsum = total
+    vat = totalsum * (13/100)
+    grandTotal = totalsum + vat
+
     print("------------------------------------------------------------------------------------------")
-    print("|S.N.     |Device Name \t\t |Company Name \t\t |Quantity\t |Price \t |")
+    print(" Distributer Name:", distributername,)
     print("------------------------------------------------------------------------------------------")
+    print("|S.N.     |Device Name \t\t |Manufacturer\t\t |Quantity \t |Price\t\t |")
+    print("------------------------------------------------------------------------------------------")
+    count = 1
+    for i in choosenLaptop:
+        print ("|", count,"\t  |",i[0], "\t |", i[1], "\t         |", i[2], "\t         |", i[3],  "\t |")
+        print("------------------------------------------------------------------------------------------")
+        count+=1
+    print("Total: ", total)
+    print("Vat Total: ", vat)
+    print("Grand Total: ", grandTotal)
